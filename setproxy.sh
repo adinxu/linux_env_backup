df4e27bda2eab83e2380922370d47868b628d469
adinxu <1334585420@qq.com>
2024-07-15 23:13:30 +0800

    update setproxy.sh

--- setproxy.sh
+++ setproxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-proxy=192.168.16.201:7890
+proxy=My-mac.local:7890
 export http_proxy=http://${proxy}
 export https_proxy=http://${proxy}
 git config --global http.proxy http://${proxy}
