9a83e65559ddff1b5d785e6dfe74f26d95d157f2
weidong xu <1334585420@qq.com>
2024-07-16 00:28:53 +0800

    update setproxy.sh

--- setproxy.sh
+++ setproxy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 proxy=My-mac.local:7890
+if [ -n "$1" ];then
+    proxy="$1"
+    echo "proxy is ${proxy}"
+fi
 export http_proxy=http://${proxy}
 export https_proxy=http://${proxy}
 git config --global http.proxy http://${proxy}
