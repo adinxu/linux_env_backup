4f7fb7012a9ecc912bf5d80ce247a61e62bdc9a8
weidong xu <1334585420@qq.com>
2024-07-16 00:49:59 +0800

    support make soft link

--- make_soft_link.sh
+++ make_soft_link.sh
@@ -4,4 +4,10 @@ source ./tools.sh
 if [ -n "$1" ];then
 	ln -sf $(pwd)/$1 ../$1
 	self_print "make soft link to ../$1"
+else
+    for file in `cat ./file_to_makelink`
+    do
+	    ln -s $(pwd)/$file ../$file
+	    self_print "make soft link to ../$file"
+    done
 fi
