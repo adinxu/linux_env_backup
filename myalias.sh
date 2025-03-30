46e4bb51c09a338db3091f7a97185ac93682d023
weidong xu <1334585420@qq.com>
2025-03-30 11:23:31 +0800

    update myalias.sh

--- myalias.sh
+++ myalias.sh
@@ -13,7 +13,7 @@ alias sc="screen -DR"
 #svn
 alias imme_up="svn up --set-depth=immediates"
 alias infi_up="svn up --set-depth=infinity"
-alias show_diff="svn status|egrep '^[MA]'|awk '{print $2}'"
+alias show_diff="svn st -q|awk '{print $2}'"
 alias getcommitfile="svn st -q|awk '{print \$2}'|tee file_to_commit.txt"
 alias mycommit="svn ci --targets file_to_commit.txt -m"
 alias mydiff="svn st -q|awk '{print \$2}'|paste -s -d ' '|xargs -I {} svn diff {}"
@@ -21,7 +21,7 @@ alias mydiff="svn st -q|awk '{print \$2}'|paste -s -d ' '|xargs -I {} svn diff {
 alias cflow="cflow -l --omit-arguments --omit-symbol-names"
 
 #pi
-alias linkpi="screen /dev/tty.wchusbserial220 115200"
+alias linkpi="screen /dev/tty.wchusbserial1110 115200"
 alias connectpi="ssh pi@link.local"
 alias pserial="sudo agent-proxy 5550^5551 0 /dev/ttyUSB0,115200"
 alias openkgdb="echo ttyAMA0  > /sys/module/kgdboc/parameters/kgdboc&&echo g > /proc/sysrq-trigger"
